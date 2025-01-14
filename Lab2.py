e8952b422539048800e75e6e9206c5e44f841814
Jaxon Bampfield <Jaxbamp@gmail.com>
2025-01-14 11:53:14 -0500

    Update Lab2.py

--- Lab2.py
+++ Lab2.py
@@ -6,32 +6,41 @@
 #Purpose: Program computes pizzas needed and salads needed
 import math
 def main():
-    pizza = int(input("Enter number of people that want pizza: "))
-    salad = int(input("Enter number of people that want salad: "))
+    pizza = int(input("Enter number of people who want pizza: "))
+    salad = float(input("Enter number of people that want salad: "))
 
     cost_of_salad = salad * 7.99
 
-    ppp = pizza * 3
+    num_of_pizza_slices = pizza * 3
 
-    pizza_ordered = math.ceil(ppp / 12)
+    total_pizzas_ordered = math.ceil(num_of_pizza_slices / 12)
 
-    total_cost_pizza = pizza_ordered * 15.99
+    cost_of_pizzas = total_pizzas_ordered * 15.99
 
-    if pizza_ordered > 10:
-        total_cost_pizza *= (1 - 0.15)
+    if total_pizzas_ordered > 10:
+        pizza_discount = cost_of_pizzas * 0.15
+    if total_pizzas_ordered < 10:
+        pizza_discount = 0
+    if salad > 10:
+        salad_discount = cost_of_salad * 0.15
+    if salad < 10:
+        salad_discount = 0
+    
+    total_discount = pizza_discount + salad_discount
 
-    total_cost = cost_of_salad + total_cost_pizza
+    total_cost = cost_of_pizzas + cost_of_salad
 
-    delivery_charge = total_cost * 0.07
-    if delivery_charge < 20:
-        delivery_charge = 20
+    delivery_fee = total_cost * 0.07
 
-    total_cost_with_delivery = total_cost + delivery_charge
+    if delivery_fee < 20:
+        delivery_fee = 20
 
-    print("Pizzas Ordered:" , pizza_ordered)
-    print("Total Pizza Cost:" , total_cost_pizza)
-    print("Salad Cost:" , cost_of_salad)
-    print("Delivery Charge:" , delivery_charge)
-    print("Total Cost:" , total_cost_with_delivery)
-
+    total_cost_all = total_cost + delivery_fee - total_discount
+    print("Pizzas Ordered:" , total_pizzas_ordered)
+    print("Pizza cost $" , cost_of_pizzas)
+    print("Salad cost $" , cost_of_salad)
+    print("Total $" , total_cost )
+    print("Discount $" , total_discount)
+    print("delivery fee $" , delivery_fee)
+    print("Total amount due $" , total_cost_all)
 main()
